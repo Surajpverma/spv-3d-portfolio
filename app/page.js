87ea1ee5ac3242343bfef5b1cd4f61b4c1d985ef
Suraj Pal <98751255+Surajpverma@users.noreply.github.com>
2023-08-05 16:54:19 +0530

    bug fix

--- app/page.js
+++ app/page.js
@@ -12,9 +12,10 @@ export default function Home() {
   return (
     <main className="w-full h-full fixed top-0 left-0 bg-[#80002F]">
 
-        {started && <CanvasThree />}
+        {/* {started && <CanvasThree />} */}
+        <CanvasThree />
 
-      <LoadingScreen started={started} setStarted={setStarted} />
+      {/* <LoadingScreen started={started} setStarted={setStarted} /> */}
     </main>
   )
 }
